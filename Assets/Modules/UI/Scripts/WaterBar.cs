7ca18505d1305c5c35fae42ce80e01a33d844f18
AndreevWezom <andreev.n.wezom@gmail.com>
2023-01-31 23:46:00 +0200

    Add water bar

---
+++ Assets/Modules/UI/Scripts/WaterBar.cs
@@ -0,0 +1,34 @@
+using System;
+using Modules.Services;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Modules.UI.Scripts
+{
+    public class WaterBar : MonoBehaviour
+    {
+        [SerializeField] private float _maxOffset;
+        [SerializeField] private Image _bar;
+        [SerializeField] private float _maxValue;
+
+        private Vector2 _startPosition;
+
+        private void Start()
+        {
+            _startPosition = _bar.rectTransform.anchoredPosition;
+        }
+
+        private void OnEnable() =>
+            ResourcesService.Water.OnChange += OnWaterChange;
+
+        private void OnDisable() =>
+            ResourcesService.Water.OnChange -= OnWaterChange;
+
+        private void OnWaterChange(float value)
+        {
+            float percent = Mathf.Clamp(value / _maxValue, 0, 1);
+            _bar.fillAmount = percent;
+            _bar.rectTransform.anchoredPosition = _startPosition + Vector2.left * _maxOffset * (1 - percent);
+        }
+    }
+}
