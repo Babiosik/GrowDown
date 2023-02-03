0f197fb88a6ef65562e0eac102c8959edbb4dc5b
AndreevWezom <andreev.n.wezom@gmail.com>
2023-02-03 19:44:01 +0200

    Add plant drink

--- Assets/Modules/Plants/Scripts/PlantLevel.cs
+++ Assets/Modules/Plants/Scripts/PlantLevel.cs
@@ -1,3 +1,4 @@
+using Modules.Services;
 using UnityEngine;
 
 namespace Modules.Plants.Scripts
@@ -5,6 +6,7 @@ namespace Modules.Plants.Scripts
     [RequireComponent(typeof(SpriteRenderer))]
     public class PlantLevel : MonoBehaviour
     {
+        [SerializeField] private float _waterPerSec;
         [SerializeField] private Sprite _dead;
         private Animator _animator;
 
@@ -14,6 +16,12 @@ namespace Modules.Plants.Scripts
         {
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (!_animator.enabled) return;
+            ResourcesService.Water.Value -= _waterPerSec * Time.deltaTime;
         }
 
         public void Die()
